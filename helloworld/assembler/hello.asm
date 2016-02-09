51d82f9b21dc8b2313eac81efa839dd22ec3e028
Tobias <chillertobi13@web.de>
2016-02-09 11:54:31 +0100

    Clean Assembler Makefile and all changed to 32bit

--- helloworld/assembler/hello.asm
+++ helloworld/assembler/hello.asm
@@ -1,6 +1,5 @@
 ; ----------------------------------------------------------------------------------------
-; To assemble and run:
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
+; Hello World
 ; ----------------------------------------------------------------------------------------
 
         global  _start
@@ -8,15 +7,15 @@
         section .text
 _start:
         ; write(1, message, 13)
-        mov     rax, 1                  ; system call 1 is write
-        mov     rdi, 1                  ; file handle 1 is stdout
-        mov     rsi, message            ; address of string to output
-        mov     rdx, 13                 ; number of bytes
-        syscall                         ; invoke operating system to do the write
+        mov     eax, 4                  ; system call 1 is write
+        mov     ebx, 1                  ; file handle 1 is stdout
+        mov     ecx, message            ; address of string to output
+        mov     edx, 14                 ; number of bytes
+        int 80h                         ; invoke operating system to do the write
 
         ; exit(0)
-        mov     eax, 60                 ; system call 60 is exit
-        xor     rdi, rdi                ; exit code 0
-        syscall                         ; invoke operating system to exit
+        mov     eax, 1                 ; system call 60 is exit
+        xor     ebx, ebx                ; exit code 0
+        int 80h                         ; invoke operating system to exit
 message:
-        db      "Hello, World", 10      ; note the newline at the end
+        db      "Hello, World!", 10      ; note the newline at the end
