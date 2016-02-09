ae59c65ec2145991a33630e15461f026301ef809
Tobias <chillertobi13@web.de>
2016-02-09 13:55:46 +0100

    Valid Jump for infitiy Hello Wordl

--- tobi/helloworld/assembler/hello.asm
+++ tobi/helloworld/assembler/hello.asm
@@ -2,16 +2,17 @@
 ; Hello World
 ; ----------------------------------------------------------------------------------------
 
-        global  main
+        global  _start
 
         section .text
-main:
+_start:
         ; write(1, message, 13)
         mov     eax, 4                  ; system call 1 is write
         mov     ebx, 1                  ; file handle 1 is stdout
         mov     ecx, message            ; address of string to output
         mov     edx, 14                 ; number of bytes
         int 80h                         ; invoke operating system to do the write
+        jmp $-16h
 
         ; exit(0)
         mov     eax, 1                 ; system call 60 is exit
