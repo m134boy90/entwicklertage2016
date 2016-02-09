06587c141e7e60f5d855f7bf162ea4f142a19f0e
Roman Bendt <roya@cryptolab.net>
2016-02-09 14:05:49 +0100

    forwardjump

---
+++ roya/forwardjump/src.asm
@@ -0,0 +1,21 @@
+; Befehl zum Assemblen: nasm -felf hello.s && ld -o hello hello.o
+
+    section data
+hello: db  "Hallo Welt!",0xa
+len:   equ $-hello
+
+    section text
+    global  _start
+_start:
+    mov eax, 4 ; write(stdout, hello, len)
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, len
+    int 80h
+
+	jmp $-16h
+
+    mov eax, 1 ; exit(0)
+    mov ebx, 0
+    int 80h
+
